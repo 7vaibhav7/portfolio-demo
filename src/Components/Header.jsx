9cee2a50490e35898ac7898ccbb453e514505ad9
VAIBHAV ARORA <145248411+7vaibhav7@users.noreply.github.com>
2024-05-23 07:52:09 +0000

    new commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,11 +18,12 @@ const Header = () => {
         padding: "1rem",
         top: 0,
         width: "100%",
-        zIndex: 10,
+       
+        zIndex: 10 
       }}
     >
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
+      <a href="#home" >Home</a>
+      <a href="#about" >About</a>
       <a href="#portfolio">Portfolio</a>
       <a href="#footer">Contact</a>
     </div>
